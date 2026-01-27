8b779ba40d873e6080ca21e28cbc7f49775d1d36
66010914078-prog <66010914078@msu.ac.th>
2026-01-27 11:22:20 +0700

    a

--- g/connectdb.php
+++ g/connectdb.php
@@ -2,7 +2,7 @@
   
 			$host = "localhost";
 			$user = "root";
-			$pwd="";
+			$pwd="Ploymanee";
 			$db="4078db";
 			$conn= mysqli_connect($host,$user,$pwd,$db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 			mysqli_query($conn,"SET NAMEs utf8");
